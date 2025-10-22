ed6e72662ad26668fb490a4cfb3245a89f9440e2
Nikita Bhatia <nikita.bhatia@saaslabs.co>
2025-10-22 11:14:49 +0530

    chore: add Swagger setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-  
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);  
-  await app.listen(process.env.PORT ?? 3001);
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager API')
+    .setDescription('API documentation for Task Manager')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // URL: /api
+
+  await app.listen(3001);
 }
 bootstrap();
